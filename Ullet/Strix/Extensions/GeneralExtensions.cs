ullet/some_csharp_code

Execute action against an instance

Ullet/Strix/Generic/GeneralExtensions.cs
@@ -1,0 +1,37 @@
+﻿/*
+ * Written by Trevor Barnett, <mr.ullet@gmail.com>, 2015
+ * Released to the Public Domain.  See http://unlicense.org/ or the
+ * UNLICENSE file accompanying this source code.
+ */
+
+using System;
+
+namespace Ullet.Strix.Generic
+{
+  /// <summary>
+  /// General extension methods for any type.
+  /// </summary>
+  public static class GeneralExtensions
+  {
+    /// <summary>
+    /// Execute an action against an instance of an object and return the
+    /// original instance.
+    /// </summary>
+    public static T InstanceExecute<T>(this T t, Action<T> action)
+    {
+      action(t);
+      return t;
+    }
+
+    /// <summary>
+    /// Execute an action against an instance of an object with an additional
+    /// object and then return the original instance.
+    /// </summary>
+    public static TInst InstanceExecuteWithObject<TInst, TObj>(
+      this TInst t, TObj o, Action<TInst, TObj> action)
+    {
+      action(t, o);
+      return t;
+    }
+  }
+}

Ullet/Strix/Tests/Unit/Generic/GeneralExtensionsTests/InstanceExecuteTests.cs
@@ -1,0 +1,50 @@
+﻿/*
+ * Written by Trevor Barnett, <mr.ullet@gmail.com>, 2015
+ * Released to the Public Domain.  See http://unlicense.org/ or the
+ * UNLICENSE file accompanying this source code.
+ */
+
+using System;
+using NUnit.Framework;
+
+namespace Ullet.Strix.Generic.Tests.Unit.GeneralExtensionsTests
+{
+  [TestFixture]
+  public class InstanceExecuteTests
+  {
+    [Test]
+    public void CanExecuteActionOnInstance()
+    {
+      var o = new ExampleClass();
+      Action<ExampleClass> actionToExecuteOnInstance = x => x.SomeValue = 123;
+
+      o.InstanceExecute(actionToExecuteOnInstance);
+
+      Assert.That(o.SomeValue, Is.EqualTo(123));
+    }
+
+    [Test]
+    public void ReturnsInstanceAfterExecutingActione()
+    {
+      var o = new ExampleClass();
+      Action<ExampleClass> actionToExecuteOnInstance = x => x.SomeValue = 123;
+
+      var instance = o.InstanceExecute(actionToExecuteOnInstance);
+
+      Assert.That(instance, Is.SameAs(o));
+    }
+
+    [Test]
+    public void CanCombineCreationAndExecutionToAvoidTemporaryVariables()
+    {
+      Assert.That(
+        new ExampleClass().InstanceExecute(x => x.SomeValue = 123).SomeValue,
+        Is.EqualTo(123));
+    }
+
+    private class ExampleClass
+    {
+      public int SomeValue { get; set; }
+    }
+  }
+}

Ullet/Strix/Tests/Unit/Generic/GeneralExtensionsTests/InstanceExecuteWithObjectTests.cs
@@ -1,0 +1,50 @@
+using System;
+using NUnit.Framework;
+
+namespace Ullet.Strix.Generic.Tests.Unit.GeneralExtensionsTests
+{
+  [TestFixture]
+  public class InstanceExecuteWithObjectTests
+  {
+    [Test]
+    public void CanExecuteActionWithObjectOnInstance()
+    {
+      var o = new ExampleClass();
+      Action<ExampleClass, int> actionToExecuteOnInstance =
+        (x, n) => x.SomeValue = 123 + n;
+
+      o.InstanceExecuteWithObject(111, actionToExecuteOnInstance);
+
+      Assert.That(o.SomeValue, Is.EqualTo(234));
+    }
+
+    [Test]
+    public void ReturnsInstanceAfterExecutingActione()
+    {
+      var o = new ExampleClass();
+      Action<ExampleClass, int> actionToExecuteOnInstance =
+        (x, n) => x.SomeValue = 123 + n;
+
+      var instance =
+        o.InstanceExecuteWithObject(111, actionToExecuteOnInstance);
+
+      Assert.That(instance, Is.SameAs(o));
+    }
+
+    [Test]
+    public void CanCombineCreationAndExecutionToAvoidTemporaryVariables()
+    {
+      Assert.That(
+        new ExampleClass()
+          .InstanceExecuteWithObject(
+            111, (x, n) => x.SomeValue = 123 + n)
+          .SomeValue,
+        Is.EqualTo(234));
+    }
+
+    private class ExampleClass
+    {
+      public int SomeValue { get; set; }
+    }
+  }
+}

ullet/Ullet.Strix.Extensions

Ullet/Strix/Extensions/GeneralExtensions.cs
@@ -1,0 +1,37 @@
+﻿/*
+ * Written by Trevor Barnett, <mr.ullet@gmail.com>, 2015
+ * Released to the Public Domain.  See http://unlicense.org/ or the
+ * UNLICENSE file accompanying this source code.
+ */
+
+using System;
+
+namespace Ullet.Strix.Extensions
+{
+  /// <summary>
+  /// General extension methods for any type.
+  /// </summary>
+  public static class GeneralExtensions
+  {
+    /// <summary>
+    /// Execute an action against an instance of an object and return the
+    /// original instance.
+    /// </summary>
+    public static T InstanceExecute<T>(this T t, Action<T> action)
+    {
+      action(t);
+      return t;
+    }
+
+    /// <summary>
+    /// Execute an action against an instance of an object with an additional
+    /// object and then return the original instance.
+    /// </summary>
+    public static TInst InstanceExecuteWithObject<TInst, TObj>(
+      this TInst t, TObj o, Action<TInst, TObj> action)
+    {
+      action(t, o);
+      return t;
+    }
+  }
+}

Ullet/Strix/Tests/Unit/Extensions/GeneralExtensionsTests/InstanceExecuteTests.cs
@@ -1,0 +1,50 @@
+﻿/*
+ * Written by Trevor Barnett, <mr.ullet@gmail.com>, 2015
+ * Released to the Public Domain.  See http://unlicense.org/ or the
+ * UNLICENSE file accompanying this source code.
+ */
+
+using System;
+using NUnit.Framework;
+
+namespace Ullet.Strix.Extensions.Tests.Unit.GeneralExtensionsTests
+{
+  [TestFixture]
+  public class InstanceExecuteTests
+  {
+    [Test]
+    public void CanExecuteActionOnInstance()
+    {
+      var o = new ExampleClass();
+      Action<ExampleClass> actionToExecuteOnInstance = x => x.SomeValue = 123;
+
+      o.InstanceExecute(actionToExecuteOnInstance);
+
+      Assert.That(o.SomeValue, Is.EqualTo(123));
+    }
+
+    [Test]
+    public void ReturnsInstanceAfterExecutingActione()
+    {
+      var o = new ExampleClass();
+      Action<ExampleClass> actionToExecuteOnInstance = x => x.SomeValue = 123;
+
+      var instance = o.InstanceExecute(actionToExecuteOnInstance);
+
+      Assert.That(instance, Is.SameAs(o));
+    }
+
+    [Test]
+    public void CanCombineCreationAndExecutionToAvoidTemporaryVariables()
+    {
+      Assert.That(
+        new ExampleClass().InstanceExecute(x => x.SomeValue = 123).SomeValue,
+        Is.EqualTo(123));
+    }
+
+    private class ExampleClass
+    {
+      public int SomeValue { get; set; }
+    }
+  }
+}

Ullet/Strix/Tests/Unit/Extensions/GeneralExtensionsTests/InstanceExecuteWithObjectTests.cs
@@ -1,0 +1,50 @@
+using System;
+using NUnit.Framework;
+
+namespace Ullet.Strix.Extensions.Tests.Unit.GeneralExtensionsTests
+{
+  [TestFixture]
+  public class InstanceExecuteWithObjectTests
+  {
+    [Test]
+    public void CanExecuteActionWithObjectOnInstance()
+    {
+      var o = new ExampleClass();
+      Action<ExampleClass, int> actionToExecuteOnInstance =
+        (x, n) => x.SomeValue = 123 + n;
+
+      o.InstanceExecuteWithObject(111, actionToExecuteOnInstance);
+
+      Assert.That(o.SomeValue, Is.EqualTo(234));
+    }
+
+    [Test]
+    public void ReturnsInstanceAfterExecutingActione()
+    {
+      var o = new ExampleClass();
+      Action<ExampleClass, int> actionToExecuteOnInstance =
+        (x, n) => x.SomeValue = 123 + n;
+
+      var instance =
+        o.InstanceExecuteWithObject(111, actionToExecuteOnInstance);
+
+      Assert.That(instance, Is.SameAs(o));
+    }
+
+    [Test]
+    public void CanCombineCreationAndExecutionToAvoidTemporaryVariables()
+    {
+      Assert.That(
+        new ExampleClass()
+          .InstanceExecuteWithObject(
+            111, (x, n) => x.SomeValue = 123 + n)
+          .SomeValue,
+        Is.EqualTo(234));
+    }
+
+    private class ExampleClass
+    {
+      public int SomeValue { get; set; }
+    }
+  }
+}