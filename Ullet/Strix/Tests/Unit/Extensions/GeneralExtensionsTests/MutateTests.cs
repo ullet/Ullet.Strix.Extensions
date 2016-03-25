ullet/some_csharp_code

Rename to Mutate because it does

Ullet/Strix/Tests/Unit/Generic/GeneralExtensionsTests/MutateTests.cs
@@ -1,0 +1,39 @@
+﻿/*
+ * Written by Trevor Barnett, <mr.ullet@gmail.com>, 2015
+ * Released to the Public Domain.  See http://unlicense.org/ or the
+ * UNLICENSE file accompanying this source code.
+ */
+
+using System;
+using NUnit.Framework;
+
+namespace Ullet.Strix.Generic.Tests.Unit.GeneralExtensionsTests
+{
+  [TestFixture]
+  public class MutateTests
+  {
+    [Test]
+    public void CanMutateInstance()
+    {
+      var o = new ExampleClass();
+      Action<ExampleClass> mutateAction = x => x.SomeValue = 123;
+
+      var instance = o.Mutate(mutateAction);
+
+      Assert.That(o.SomeValue, Is.EqualTo(123));
+      Assert.That(instance, Is.SameAs(o));
+    }
+
+    [Test]
+    public void CanUseMutateAsAnInitializer()
+    {
+      var o = new ExampleClass().Mutate(x => x.SomeValue = 123);
+      Assert.That(o.SomeValue, Is.EqualTo(123));
+    }
+
+    private class ExampleClass
+    {
+      public int SomeValue { get; set; }
+    }
+  }
+}

Ullet/Strix/Tests/Unit/Generic/GeneralExtensionsTests/MutateWithObjectTests.cs
@@ -1,0 +1,40 @@
+/*
+ * Written by Trevor Barnett, <mr.ullet@gmail.com>, 2015
+ * Released to the Public Domain.  See http://unlicense.org/ or the
+ * UNLICENSE file accompanying this source code.
+ */
+
+using System;
+using NUnit.Framework;
+
+namespace Ullet.Strix.Generic.Tests.Unit.GeneralExtensionsTests
+{
+  [TestFixture]
+  public class MutateWithObjectTests
+  {
+    [Test]
+    public void CanMutateInstanceWithAnotherObject()
+    {
+      var o = new ExampleClass();
+      Action<ExampleClass, int> mutateAction = (x, n) => x.SomeValue = 123 + n;
+
+      var instance = o.MutateWithObject(111, mutateAction);
+
+      Assert.That(o.SomeValue, Is.EqualTo(234));
+      Assert.That(instance, Is.SameAs(o));
+    }
+
+    [Test]
+    public void CanUseMutateWithObjectAsAnInitializer()
+    {
+      var o = new ExampleClass()
+        .MutateWithObject(111, (x, n) => x.SomeValue = 123 + n);
+      Assert.That(o.SomeValue, Is.EqualTo(234));
+    }
+
+    private class ExampleClass
+    {
+      public int SomeValue { get; set; }
+    }
+  }
+}

ullet/Ullet.Strix.Extensions

Ullet/Strix/Tests/Unit/Extensions/GeneralExtensionsTests/MutateTests.cs
@@ -1,0 +1,39 @@
+﻿/*
+ * Written by Trevor Barnett, <mr.ullet@gmail.com>, 2015
+ * Released to the Public Domain.  See http://unlicense.org/ or the
+ * UNLICENSE file accompanying this source code.
+ */
+
+using System;
+using NUnit.Framework;
+
+namespace Ullet.Strix.Extensions.Tests.Unit.GeneralExtensionsTests
+{
+  [TestFixture]
+  public class MutateTests
+  {
+    [Test]
+    public void CanMutateInstance()
+    {
+      var o = new ExampleClass();
+      Action<ExampleClass> mutateAction = x => x.SomeValue = 123;
+
+      var instance = o.Mutate(mutateAction);
+
+      Assert.That(o.SomeValue, Is.EqualTo(123));
+      Assert.That(instance, Is.SameAs(o));
+    }
+
+    [Test]
+    public void CanUseMutateAsAnInitializer()
+    {
+      var o = new ExampleClass().Mutate(x => x.SomeValue = 123);
+      Assert.That(o.SomeValue, Is.EqualTo(123));
+    }
+
+    private class ExampleClass
+    {
+      public int SomeValue { get; set; }
+    }
+  }
+}

Ullet/Strix/Tests/Unit/Extensions/GeneralExtensionsTests/MutateWithObjectTests.cs
@@ -1,0 +1,40 @@
+/*
+ * Written by Trevor Barnett, <mr.ullet@gmail.com>, 2015
+ * Released to the Public Domain.  See http://unlicense.org/ or the
+ * UNLICENSE file accompanying this source code.
+ */
+
+using System;
+using NUnit.Framework;
+
+namespace Ullet.Strix.Extensions.Tests.Unit.GeneralExtensionsTests
+{
+  [TestFixture]
+  public class MutateWithObjectTests
+  {
+    [Test]
+    public void CanMutateInstanceWithAnotherObject()
+    {
+      var o = new ExampleClass();
+      Action<ExampleClass, int> mutateAction = (x, n) => x.SomeValue = 123 + n;
+
+      var instance = o.MutateWithObject(111, mutateAction);
+
+      Assert.That(o.SomeValue, Is.EqualTo(234));
+      Assert.That(instance, Is.SameAs(o));
+    }
+
+    [Test]
+    public void CanUseMutateWithObjectAsAnInitializer()
+    {
+      var o = new ExampleClass()
+        .MutateWithObject(111, (x, n) => x.SomeValue = 123 + n);
+      Assert.That(o.SomeValue, Is.EqualTo(234));
+    }
+
+    private class ExampleClass
+    {
+      public int SomeValue { get; set; }
+    }
+  }
+}