a979fc87b6ca4579e54791fd20a5c90e0ab3e7af
Trevor Barnett <mr.ullet@gmail.com>
2016-03-26 14:03:52 +0000

    Not - negate predicate

--- Ullet/Strix/Extensions/DelegateExtensions_Not.cs
+++ Ullet/Strix/Extensions/DelegateExtensions_Not.cs
@@ -1,17 +1,64 @@
-﻿using System;
+﻿/*
+ * Written by Trevor Barnett, <mr.ullet@gmail.com>, 2016
+ * Released to the Public Domain.  See http://unlicense.org/ or the
+ * UNLICENSE file accompanying this source code.
+ */
+
+using System;
 
 namespace Ullet.Strix.Extensions
 {
   public static partial class DelegateExtensions
   {
+    /// <summary>
+    /// Return negative predicate.
+    /// </summary>
     public static Func<bool> Not(this Func<bool> f)
     {
       return () => !f();
     }
 
+    /// <summary>
+    /// Return negative predicate.
+    /// </summary>
     public static Func<T, bool> Not<T>(this Func<T, bool> f)
     {
       return t => !f(t);
+    }
+
+    /// <summary>
+    /// Return negative predicate.
+    /// </summary>
+    public static Func<T1, T2, bool> Not<T1, T2>(this Func<T1, T2, bool> f)
+    {
+      return (t1, t2) => !f(t1, t2);
+    }
+
+    /// <summary>
+    /// Return negative predicate.
+    /// </summary>
+    public static Func<T1, T2, T3, bool> Not<T1, T2, T3>(
+      this Func<T1, T2, T3, bool> f)
+    {
+      return (t1, t2, t3) => !f(t1, t2, t3);
+    }
+
+    /// <summary>
+    /// Return negative predicate.
+    /// </summary>
+    public static Func<T1, T2, T3, T4, bool> Not<T1, T2, T3, T4>(
+      this Func<T1, T2, T3, T4, bool> f)
+    {
+      return (t1, t2, t3, t4) => !f(t1, t2, t3, t4);
+    }
+
+    /// <summary>
+    /// Return negative predicate.
+    /// </summary>
+    public static Func<T1, T2, T3, T4, T5, bool> Not<T1, T2, T3, T4, T5>(
+      this Func<T1, T2, T3, T4, T5, bool> f)
+    {
+      return (t1, t2, t3, t4, t5) => !f(t1, t2, t3, t4, t5);
     }
   }
 }
---
+++ Ullet/Strix/Tests/Unit/Extensions/DelegateExtensions/NotTests.cs
@@ -0,0 +1,69 @@
+﻿/*
+ * Written by Trevor Barnett, <mr.ullet@gmail.com>, 2016
+ * Released to the Public Domain.  See http://unlicense.org/ or the
+ * UNLICENSE file accompanying this source code.
+ */
+
+using NUnit.Framework;
+using Fn = Ullet.Strix.Extensions.DelegateExtensions;
+
+namespace Ullet.Strix.Extensions.Tests.Unit.DelegateExtensions
+{
+  [TestFixture]
+  public class NotTests
+  {
+    [Test]
+    public void CanNegateZeroParameterReturnBoolFunction()
+    {
+      Assert.That(Fn.Not(() => true)(), Is.False);
+      Assert.That(Fn.Not(() => false)(), Is.True);
+    }
+
+    [Test]
+    public void CanNegateOneParameterPredicate()
+    {
+      Assert.That(Fn.Not(() => true)(), Is.False);
+      Assert.That(Fn.Not(() => false)(), Is.True);
+    }
+
+    [Test]
+    public void CanNegateTwoParameterPredicate()
+    {
+      Assert.That(Fn.Not((int a, long b) => a > b)(2, 1), Is.False);
+      Assert.That(Fn.Not((int a, long b) => a <= b)(2, 1), Is.True);
+    }
+
+    [Test]
+    public void CanNegateThreeParameterPredicate()
+    {
+      Assert.That(
+        Fn.Not((double a, int b, long c) => a / b > c)(3, 2, 1), Is.False);
+      Assert.That(
+        Fn.Not((double a, int b, long c) => a / b <= c)(3, 2, 1), Is.True);
+    }
+
+    [Test]
+    public void CanNegateFourParameterPredicate()
+    {
+      Assert.That(
+        Fn.Not((double a, int b, long c, byte d) => a / b > c + d)(7, 2, 1, 2),
+        Is.False);
+      Assert.That(
+        Fn.Not((double a, int b, long c, byte d) => a / b <= c + d)(7, 2, 1, 2),
+        Is.True);
+    }
+
+    [Test]
+    public void CanNegateFiveParameterPredicate()
+    {
+      Assert.That(
+        Fn.Not((double a, int b, long c, byte d, float e) =>
+          a / b > (c + d) / e)(7.0, 2, 5, 1, 3.0f),
+        Is.False);
+      Assert.That(
+        Fn.Not((double a, int b, long c, byte d, float e) =>
+          a / b <= (c + d) / e)(7.0, 2, 5, 1, 3.0f),
+        Is.True);
+    }
+  }
+}
